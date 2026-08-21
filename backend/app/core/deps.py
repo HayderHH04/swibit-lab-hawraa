a55ee49cb34571eac7e01b16c69094d02f954701
hawraa <hawraaasaad99@gmail.com>
2026-08-22 01:06:26 +0300

    feat: implement authentication and dependencies

--- backend/app/core/deps.py
+++ backend/app/core/deps.py
@@ -5,11 +5,11 @@ from sqlalchemy.orm import Session
 
 from app.core.database import get_db
 from app.core.security import SECRET_KEY, ALGORITHM
-from app.models.models import User
+import models , schemas
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/users/login")
 
-def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)) -> User:
+def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)) ->models.User:
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
         detail="Could not validate credentials",
@@ -23,7 +23,11 @@ def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(
     except jwt.PyJWTError:
         raise credentials_exception
 
-    user = db.query(User).filter(User.id == int(user_id)).first()
+    user = db.query(models.User).filter(models.User.id == int(user_id)).first()
     if user is None:
         raise credentials_exception
     return user
+def get_current_admin_user(current_user:models.User = Depends(get_current_user) ):
+    if not current_user.is_admin:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,detail="Access denied: Admins only")  
+    return current_user
--- backend/app/core/security.py
+++ backend/app/core/security.py
@@ -16,7 +16,7 @@ def hash_password(password: str) -> str:
     return pwd_context.hash(password[:72])
 
 def verify_password(password: str, hashed_password: str) -> bool:
-    return pwd_context.verify(password, hashed_password)
+    return pwd_context.verify(password[:72], hashed_password)
 
 def create_access_token(data: dict) -> str:
     to_encode = data.copy()
